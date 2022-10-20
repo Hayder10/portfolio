68987ed88bad7a00da044220ed1697e24e1afef2
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-10-19 22:29:28 -0300

    finished interests and contact section

--- src/Contact.js
+++ src/Contact.js
@@ -1,8 +1,29 @@
 import React from 'react'
 
 function Contact() {
+  const handleClick = (url) => {
+    window.open(url,'__blank','noopener,noreferrer');
+  }
   return (
-    <div>Contact</div>
+    <div className='contact container d-flex flex-column justify-content-center'>
+      <div className="row">
+        <div className="col-md-12">
+          <h1 className="display-6">
+          If you have anything to tell me, I'll leave my social media here!
+          </h1>
+        </div>
+        <div className="col-md-4 pt-5">
+          <h1 className='display-6'><i className="fa-brands fa-2xl fa-linkedin" onClick={() => {handleClick('https://www.linkedin.com/in/rodrigo-cabrera10/')}}></i></h1>
+        </div>
+        <div className="col-md-4 pt-5">
+          <h1 className='display-6'><i className="fa-brands fa-2xl fa-square-github" onClick={() => {handleClick('https://github.com/Hayder10')}}></i></h1>
+        </div>
+        <div className="col-md-4 pt-5">
+          <h1 className='display-6'><i className="fa-brands fa-2xl fa-square-instagram" onClick={() => {handleClick('https://www.instagram.com/rodrigo_hayder10/')}}></i></h1>
+        </div>
+        
+      </div>
+    </div>
   )
 }
 
--- src/Interests.js
+++ src/Interests.js
@@ -3,18 +3,20 @@ import React from 'react'
 function Interests() {
   return (
     <div className='interests container d-flex flex-column justify-content-center'>
-      <div className="row">
+      <div className="row g-5">
         <div className="col-md-12">
-          <h1 className="display-6">
-            Let me talk about myself a little bit... <br />
-            I'm an absolute nerd, I love getting into stuff and start learning very deeply about what I'm interested in.
+          <h1 className="display-6 fst-italic">
+            Let me talk about myself a little bit...
           </h1>
         </div>
-        <div className="col-md-6 text-start">
-          <h1 className="display-6">My main goal right now is to keep growing as a developer, learning from all of my experienced peers</h1>
+        <div className="col-md-12">
+          <h1 className="display-6">I'm an absolute nerd, I love getting into stuff and start learning very deeply about what I'm interested in.</h1>
         </div>
-        <div className="col-md-6 text-end">
-          <h1 className="display-6">I'd love to learn Node!</h1>
+        <div className="col-md-12">
+          <h1 className="display-6">I like videogames, philosophy and Martial Arts.</h1>
+        </div>
+        <div className="col-md-12">
+          <h1 className="display-6">You can talk to me about whatever thing comes to your mind, I love listening to people!.</h1>
         </div>
       </div>
     </div>
