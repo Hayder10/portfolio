ec3539b04318398c0f2ef50268f63b31c3313f57
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-10-24 18:27:13 -0300

    fixed overscroll on phone resolution

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -5,17 +5,15 @@ import profile from './media/hayderkazuya.jpg'
 function Dashboard() {
   return (
     <div className='dashboard container-fluid d-flex align-items-center'>
-      <div className='container-fluid'>
         <div className='row'>
-          <div className='col-md-8 d-flex flex-column justify-content-center'>
+          <div className='col-md-6 align-self-center'>
               <h2 className='display-3'>Hi! , I'm</h2>
               <h1 className='display-1'>Rodrigo Cabrera</h1>
               <h2 className='display-5 fst-italic'>Full Stack Developer</h2>
           </div>
-          <div className='col-md-4 py-5 d-flex justify-content-center'>
-            <img src={profile} className='img-fluid rounded-circle py-5' alt='error!'></img>
+          <div className='col-md-5'>
+            <img src={profile} className='img-fluid rounded-circle' alt='error!'></img>
           </div>
-        </div>
       </div>
     </div>
   )
