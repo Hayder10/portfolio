de43762ebff7386d7160b57d68dec328d639a8ac
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-10-24 19:06:22 -0300

    added typewriter effect

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,6 +1,7 @@
 import React from 'react'
 import './index.css';
 import profile from './media/hayderkazuya.jpg'
+import { Typewriter } from 'react-simple-typewriter';
 
 function Dashboard() {
   return (
@@ -8,10 +9,10 @@ function Dashboard() {
         <div className='row'>
           <div className='col-md-6 align-self-center'>
               <h2 className='display-3'>Hi! , I'm</h2>
-              <h1 className='display-1'>Rodrigo Cabrera</h1>
+              <h1 className='display-1'><Typewriter loop cursor delaySpeed={2000} words={['Rodrigo Cabrera','Hayder']}/></h1>
               <h2 className='display-5 fst-italic'>Full Stack Developer</h2>
           </div>
-          <div className='col-md-5'>
+          <div className='col-md-5 p-5'>
             <img src={profile} className='img-fluid rounded-circle' alt='error!'></img>
           </div>
       </div>
