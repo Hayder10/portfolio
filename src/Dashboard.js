bf985b3c9fa2f9a4dbede9b76175fdb4ea8edab9
Hayder10 <rodrigo.cabrera.catemu10@gmail.com>
2022-11-07 15:00:53 -0300

    fixed image and typewriter moving the image

--- src/Dashboard.js
+++ src/Dashboard.js
@@ -1,15 +1,15 @@
 import React from 'react'
 import './index.css';
-import profile from './media/hayderkazuya.jpg'
+import profile from './media/yo.jpg'
 import { Typewriter } from 'react-simple-typewriter';
 
 function Dashboard() {
   return (
     <div className='dashboard container-fluid d-flex align-items-center'>
-        <div className='row'>
+        <div className='row justify-content-around'>
           <div className='col-md-6 align-self-center'>
               <h2 className='display-3'>Hi! , I'm</h2>
-              <h1 className='display-1'><Typewriter loop cursor delaySpeed={2000} words={['Rodrigo Cabrera','Hayder']}/></h1>
+              <h1 className='display-1'><Typewriter loop cursor delaySpeed={2000} words={['Rodrigo','Hayder']}/></h1>
               <h2 className='display-5 fst-italic'>Full Stack Developer</h2>
           </div>
           <div className='col-md-5 p-5'>
